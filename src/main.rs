cb8942084ae687e3f688a65a89611611867cb213
overclockworked64 <overclockworked64@users.noreply.github.com>
2021-12-09 18:44:35 +0100

    fix: use BufReader to avoid cutting off messages

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    net::TcpStream,
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::{
+        tcp::{ReadHalf, WriteHalf},
+        TcpStream,
+    },
 };
 
 const NICK: &str = "toot";
@@ -8,52 +11,46 @@ const USER: &str = "tootz";
 const NETWORK: &str = "irc.libera.chat";
 const PORT: u16 = 6667;
 
-const TERMINATOR_LENGTH: usize = 2;
-
-async fn send(stream: &mut TcpStream, message: &str) {
+async fn send(stream: &mut WriteHalf<'_>, message: &str) {
     let msg = format!("{}\r\n", message);
     if let Err(e) = stream.write(msg.as_bytes()).await {
         eprintln!("writing to stream failed: {}", e);
     }
 }
 
-async fn recv_msg(stream: &mut TcpStream, buf: &mut [u8]) -> String {
-    while !String::from_utf8(buf.to_vec()).unwrap().contains("\r\n") {
-        let _ = stream.read(buf).await.unwrap();
+async fn recv_msg(reader: &mut BufReader<ReadHalf<'_>>, buf: &mut Vec<u8>) -> Option<String> {
+    if let Err(e) = reader.read_until(b'\n', buf).await {
+        eprintln!("reading from stream failed: {}", e);
+
+        None
+    } else {
+        let msg = String::from_utf8(buf.to_vec()).unwrap();
+
+        Some(msg.trim().to_owned())
     }
-
-    let chunk = String::from_utf8(buf.to_vec()).unwrap();
-    let terminator_index = chunk.find("\r\n").unwrap();
-    let message = chunk
-        .chars()
-        .take(terminator_index + TERMINATOR_LENGTH)
-        .filter(|x| *x != '\0')
-        .collect::<String>();
-
-    for i in 0..terminator_index + TERMINATOR_LENGTH {
-        buf[i] = 0;
-    }
-
-    message
 }
 
 #[tokio::main]
 async fn main() {
     let mut stream = TcpStream::connect((NETWORK, PORT)).await.unwrap();
+    let (rx, mut tx) = stream.split();
 
-    send(&mut stream, format!("NICK {}", NICK).as_str()).await;
-    send(&mut stream, format!("USER {} 0 * :{}", USER, USER).as_str()).await;
+    send(&mut tx, format!("NICK {}", NICK).as_str()).await;
+    send(&mut tx, format!("USER {} 0 * :{}", USER, USER).as_str()).await;
 
-    let mut buf = [0u8; 2048];
+    let mut buf = vec![0u8; 8192];
+    let mut reader = BufReader::new(rx);
 
     loop {
-        let message = recv_msg(&mut stream, &mut buf).await;
+        buf.clear();
 
-        println!("{}", message);
+        if let Some(message) = recv_msg(&mut reader, &mut buf).await {
+            println!("{}", message);
 
-        if message.starts_with("PING") {
-            let reply = message.replace("PING", "PONG");
-            send(&mut stream, &reply).await;
+            if message.starts_with("PING") {
+                let reply = message.replace("PING", "PONG");
+                send(&mut tx, &reply).await;
+            }
         }
     }
 }
