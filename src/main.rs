58fc1943767c47aa3e99e645dfa05b62705507de
overclockworked64 <overclockworked64@users.noreply.github.com>
2021-12-12 03:48:49 +0100

    fix: don't use buffered writing feat: implement obeying the commander feat: implement joining channels

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use tokio::{
-    io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter},
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::{
         tcp::{ReadHalf, WriteHalf},
         TcpStream,
@@ -11,7 +11,13 @@ const USER: &str = "tootz";
 const NETWORK: &str = "irc.libera.chat";
 const PORT: u16 = 6667;
 
-async fn send(writer: &mut BufWriter<WriteHalf<'_>>, message: &str) {
+const COMMANDER: &str = "xvm`!~xvm@user/xvm";
+
+async fn join(writer: &mut WriteHalf<'_>, channel: &str) {
+    send(writer, format!("JOIN {}", channel).as_str()).await;    
+}
+
+async fn send(writer: &mut WriteHalf<'_>, message: &str) {
     let msg = format!("{}\r\n", message);
     if let Err(e) = writer.write(msg.as_bytes()).await {
         eprintln!("writing to stream failed: {}", e);
@@ -38,12 +44,11 @@ async fn recv_msg(reader: &mut BufReader<ReadHalf<'_>>, buf: &mut Vec<u8>) -> Op
 async fn main() {
     let mut stream = TcpStream::connect((NETWORK, PORT)).await.unwrap();
     let mut buf = vec![0u8; 8192];
-    let (rx, tx) = stream.split();
-    let mut writer = BufWriter::new(tx);
+    let (rx, mut tx) = stream.split();
     let mut reader = BufReader::new(rx);
 
-    send(&mut writer, format!("NICK {}", NICK).as_str()).await;
-    send(&mut writer, format!("USER {} 0 * :{}", USER, USER).as_str()).await;
+    send(&mut tx, format!("NICK {}", NICK).as_str()).await;
+    send(&mut tx, format!("USER {} 0 * :{}", USER, USER).as_str()).await;
 
     loop {
         buf.clear();
@@ -53,7 +58,15 @@ async fn main() {
 
             if message.starts_with("PING") {
                 let reply = message.replace("PING", "PONG");
-                send(&mut writer, &reply).await;
+                send(&mut tx, &reply).await;
+            }
+
+            if message.starts_with(format!(":{}", COMMANDER).as_str()) {
+                let command = message.split(":").nth(2).unwrap();
+                if command.starts_with("!join") {
+                    let channel = command.split(" ").nth(1).unwrap();
+                    join(&mut tx, channel).await;
+                }
             }
         }
     }
